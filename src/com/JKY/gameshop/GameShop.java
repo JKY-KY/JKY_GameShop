d5d594c6c3a4c740114e5402623d764afc4f7989
JKY <jky2381@gmail.com>
2021-05-21 17:31:30 +0900

    JKY GameShop Opening commit

---
+++ src/com/JKY/gameshop/GameShop.java
@@ -0,0 +1,10 @@
+package com.JKY.gameshop;
+
+public class GameShop {
+	public static void main(String[] args) {
+		GameType game = GameType.RPG;
+		System.out.println("JK 게임샵에 오신걸 환영합니다!");
+		System.out.println(GameType.어드벤쳐);
+		
+	}
+}
---
+++ src/com/JKY/gameshop/GameType.java
@@ -0,0 +1,5 @@
+package com.JKY.gameshop;
+
+public enum GameType {
+	RPG, 어드벤쳐, 액션, FPS, 레이싱
+}
