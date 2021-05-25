f45169e07f976566ebf69749aabef9a95a887eb6
JKY <jky2381@gmail.com>
2021-05-25 17:59:10 +0900

    게임샵 메뉴 문자열 정리

--- src/com/JKY/gameshop/GameShop.java
+++ src/com/JKY/gameshop/GameShop.java
@@ -1,10 +1,23 @@
 package com.JKY.gameshop;
 
 public class GameShop {
+	
 	public static void main(String[] args) {
 		GameType game = GameType.RPG;
 		System.out.println("JK 게임샵에 오신걸 환영합니다!");
-		System.out.println(GameType.어드벤쳐);
+		GameType gameTypes[] = GameType.values();
 		
+		/**
+		 * 나중에 Enum to List 사용하여 깔끔하게 정리
+		 */
+		String menu = String.join("/",
+				GameType.RPG.toString(),
+				GameType.어드벤쳐.toString(),
+                GameType.액션.toString(),
+                GameType.FPS.toString(),
+                GameType.레이싱.toString() );
+		
+		System.out.println(menu);
+		String[] menus = menu.split("/");
 	}
 }
