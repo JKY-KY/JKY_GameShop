6745cee43559f288f33d5b6542215384aa2dc698
JKY <jky2381@gmail.com>
2021-05-27 15:53:44 +0900

    장르 선택 스위치기능 사용

--- src/com/JKY/gameshop/GameShop.java
+++ src/com/JKY/gameshop/GameShop.java
@@ -21,11 +21,11 @@ public class GameShop {
 		 * 나중에 Enum to List 사용하여 깔끔하게 정리
 		 */
 		String menu = String.join("/",
-				GameType.RPG.toString(),
-				GameType.어드벤쳐.toString(),
-                GameType.액션.toString(),
-                GameType.FPS.toString(),
-                GameType.레이싱.toString() );
+				GameType.RPG.toString() + (GameType.RPG.ordinal() + 1),
+				GameType.어드벤쳐.toString() + (GameType.어드벤쳐.ordinal() + 1),
+                GameType.액션.toString() + (GameType.액션.ordinal() + 1),
+                GameType.FPS.toString() + (GameType.FPS.ordinal() + 1),
+                GameType.레이싱.toString() + (GameType.레이싱.ordinal() + 1));
 		
 		System.out.println(menu);
 		int idx = LocalDate.now().getDayOfYear() % gameTypes.length;
@@ -34,15 +34,38 @@ public class GameShop {
 				DateTimeFormatter.ofPattern("E") // E => Weekday
 				.withLocale(Locale.KOREAN));
 		System.out.println(weekDay + "요일의 추천장르는 " + gameTypes[idx] + "!!");
-//      GameType.RPG.ordinal()
+
 			
-		System.out.println("원하시는 게임 장르를 입력해주세요 : ");
+		System.out.println("원하시는 게임 장르의 번호를 입력해주세요 : ");
 		
 		Scanner scanner = new Scanner(System.in);
-		String 게임장르 = scanner.next();
-		System.out.println("고객님은 " + 게임장르 + " 장르를 선택하셨습니다!");
-		scanner.close();
 		
+		int 장르번호 = scanner.nextInt();
+		
+		System.out.println("고객님은 " + 장르번호 + " 번을 선택하셨습니다!");
+		
+		GameType gameType = GameType.values()[장르번호 - 1];
+		switch (gameType) {
+		case RPG:
+			System.out.println("RPG 장르를 선택하셨습니다!");
+			break;
+		case 어드벤쳐:
+			System.out.println("어드벤쳐 장르를 선택하셨습니다!");
+			break;
+		case 액션:
+			System.out.println("액션 장르를 선택하셨습니다!");
+			break;
+		case FPS:
+			System.out.println("FPS 장르를 선택하셨습니다!");
+			break;
+		case 레이싱:
+			System.out.println("레이싱 장르를 선택하셨습니다!");
+			// break;
+			default:
+				break;		
+		}
+		
+		scanner.close();
 		return;
 	}
 }
